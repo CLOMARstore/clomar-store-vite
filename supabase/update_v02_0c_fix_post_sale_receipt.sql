66941b9551c114282ba4a77655af21b7ae10f104
CLOMAR STORE <clomarstore.v@gmail.com>
2026-06-13 23:33:37 -0500

    Add files via upload

---
+++ supabase/update_v02_0c_fix_post_sale_receipt.sql
@@ -0,0 +1,3 @@
+-- V02.0C - Corrección del modal y respaldo de último comprobante post-venta
+-- No requiere migración adicional. Conserva estructura V02.0/V02.0B.
+select 'V02.0C listo: modal post-venta reforzado y respaldo de último comprobante preparado' as status;
