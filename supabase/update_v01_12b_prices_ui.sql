1a2497f5730c3b187404c83e3b58a278342017a8
CLOMAR STORE <clomarstore.v@gmail.com>
2026-06-13 22:28:22 -0500

    Add files via upload

---
+++ supabase/update_v01_12b_prices_ui.sql
@@ -0,0 +1,3 @@
+-- V01.12B - Interfaz profesional de precios
+-- No requiere migración adicional. Conserva estructura V01.12.
+select 'V01.12B listo: interfaz profesional de precios no requiere migración adicional' as status;
